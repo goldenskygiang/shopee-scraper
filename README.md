4be27e5e6d7c6b7bccb4eeea64eec2ccd153e6ed
Vu Truong Giang <giangthedev@outlook.com.vn>
2022-08-26 17:56:43 +0700

    changed email to toast notification

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Shopee Scraper
-Simple script to check product availability in Shopee when it out of stock. Once restock it will send an email notification! 
+Simple script to check product availability in Shopee when it out of stock. Once restock it will ~~send an email notification~~ send a Windows Toast notification!
--- shopee_scraper.py
+++ shopee_scraper.py
@@ -3,48 +3,32 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from time import sleep
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
+from winotify import Notification
 
 chrome_options = Options()
-# chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless")
 
-sendgrid_api_key = "<YOUR_SENDGRID_API_KEY>"
-url = 'https://shopee.com.my/product/724335/2212828002'
+url = input('Enter your Shopee URL: ')
 
-driver = webdriver.Chrome(service = Service( <YOUR_CHROMEDRIVER_EXE_PATH> ))
+driver = webdriver.Edge(service = Service('msedgedriver.exe'))
 
 driver.get(url)
 
-sleep(1)
+while True:
+    sleep(4)
+    # stock reading count on the product detail section
+    read_stock = "/html/body/div[1]/div/div[2]/div[1]/div/div[1]/div/div[2]/div[3]/div/div[4]/div/div[2]/div/div[2]/div[2]/div[2]"
+    out = driver.find_element(By.XPATH, read_stock).text
 
-# click the english language button
-driver.find_element(By.XPATH, """//*[@id="modal"]/div[1]/div[1]/div/div[3]/div[1]/button""").click()
+    if out[0] != "0":
+        print("Product is available to purchase!")
+        print(out)
 
-sleep(3)
+        toast = Notification("Product available", out)
+        toast.add_actions(label="Click here", launch=url)
+        toast.show()
+        break
 
-# stock reading count on the product detail section
-read_stock = """//*[@id="main"]/div/div[2]/div[1]/div/div[1]/div/div[2]/div[3]/div/div[4]/div/div[4]/div/div[3]/div[2]/div[2]"""
-out = driver.find_element(By.XPATH, read_stock).text
-
-if out == "0":
-    print("Product still out of stock")
-
-else:
-    print("Product is available to purchase!")
-
-    message = Mail(
-        from_email='<YOUR_SENDER_EMAIL>',
-        to_emails='<YOUR_RECEIVER_EMAIL>',
-        subject='Izzat Shopee Script - Your item is back in stock!',
-        html_content='<strong>Buy now!</strong>')
-    try:
-        sg = SendGridAPIClient(sendgrid_api_key)
-        response = sg.send(message)
-        print(response.status_code)
-        print(response.body)
-        print(response.headers)
-    except Exception as e:
-        print(e.message)
+    driver.refresh()
 
 driver.quit()
